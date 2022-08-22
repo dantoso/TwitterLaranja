2dd4622d596caef6c30a2a53df081cf57e76ca63
dantoso <xeddantasvfd@gmail.com>
2022-08-22 10:41:41 -0300

    fix: saving post content in view model correctly and adding mention to view model init

--- Posterr/Views/Components/PostCreation/PostCreationViewModel.swift
+++ Posterr/Views/Components/PostCreation/PostCreationViewModel.swift
@@ -5,16 +5,17 @@ final class PostCreationViewModel: NSObject, UITextViewDelegate {
 	let textLimit = 777
 	weak var author: User!
 	var postData = NewPostData()
-	
 	weak var counterLabel: UILabel?
 	weak var postButton: UIButton?
 	
-	init(author: User) {
+	init(author: User, mention: Post? = nil) {
 		self.author = author
+		self.postData.mention = mention
 	}
 	
 	func post() {
 		// TODO: implement post
+//		let post = Post(author: author, content: postData.content, mention: postData.mention)
 	}
 	
 	
@@ -22,14 +23,11 @@ final class PostCreationViewModel: NSObject, UITextViewDelegate {
 	func textViewDidChange(_ textView: UITextView) {
 		counterLabel?.text = "\(textLimit - textView.text.count)"
 		postButton?.isEnabled = textView.text.count > 0
+		postData.content = textView.text
 	}
 	
 	func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
 		return textView.text.count + text.count <= textLimit
-	}
-	
-	func textViewDidEndEditing(_ textView: UITextView) {
-		postData.content = textView.text
 	}
 }
 
