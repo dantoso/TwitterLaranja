482715cbc8269f6c1c324fa4afc63903fbc7868a
dantoso <xeddantasvfd@gmail.com>
2022-08-23 00:13:57 -0300

    feat: repost ui

--- Posterr/Shared/Coordinator.swift
+++ Posterr/Shared/Coordinator.swift
@@ -24,10 +24,18 @@ extension PostWriterCoordinator {
 	}
 	
 	func writeRepost(withAuthor author: User, mentioning post: Post) {
-		let viewModel = PostCreationViewModel(author: author, mention: post)
-		let writePostVC = PostCreationViewController(viewModel: viewModel)
 		
-		navigationController.present(writePostVC, animated: true)
+		let viewModel = PostCreationViewModel(author: author, mention: post)
+		
+		let alert = UIAlertController(title: "Repost confirmation",
+									  message: "Do you want to repost \(post.authorName)'s post?",
+									  preferredStyle: .alert)
+		alert.addAction(UIAlertAction(title: "Repost", style: .default, handler: { _ in
+			viewModel.post()
+		}))
+		alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+		
+		navigationController.present(alert, animated: true)
 	}
 	
 	func writeQuotePost(withAuthor author: User, mentioning post: Post) {
